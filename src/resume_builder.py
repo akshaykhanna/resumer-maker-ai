3f9a50ac3f4492b7afabf3820260719bad4996cc
akshaykhanna <akshay9911102163@gmail.com>
2023-10-19 19:35:44 +0530

    Add code to generale resume from html template

---
+++ src/resume_builder.py
@@ -0,0 +1,18 @@
+import jinja2
+import json
+from weasyprint import HTML
+
+# Load the HTML template
+template_loader = jinja2.FileSystemLoader(searchpath="./")
+template_env = jinja2.Environment(loader=template_loader)
+template = template_env.get_template("resume_template.html")
+
+# Load data from a JSON file
+with open("resume_data.json", "r") as data_file:
+    data = json.load(data_file)
+
+# Render the HTML template with the data
+html_content = template.render(data)
+
+# Create a PDF from the rendered HTML
+HTML(string=html_content).write_pdf("resume.pdf")
