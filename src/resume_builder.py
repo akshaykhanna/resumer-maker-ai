aa76a6406a0778b04e23bcddd4dd179af3fbfafe
akshay.khanna <akshay991110263@gmail.com>
2023-10-20 11:36:18 +0530

    Convert resume builder to class

--- src/resume_builder.py
+++ src/resume_builder.py
@@ -9,19 +9,30 @@ from constants import (
     RESUME_FILE
 )
 
-# Load the HTML template
-template_loader = jinja2.FileSystemLoader(searchpath="./")
-template_env = jinja2.Environment(loader=template_loader)
-template = template_env.get_template(f"{INPUT_FOLDER_PATH}{RESUME_TEMPLATE_FILE}")
+class ResumeBuilder:
+    def __init__(self, resume_template=RESUME_TEMPLATE_FILE, resume_data_file=RESUME_DATA_FILE):
+        self._load_template(resume_template)
+        self.get_resume_data(resume_data_file)
+    
+    def get_resume_data(self, resume_data_file):
+        with open(f"{INPUT_FOLDER_PATH}{resume_data_file}", "r") as data_file:
+            self.resume_data = json.load(data_file)
+        
+    def _load_template(self, resume_template):
+        # Load the HTML template
+        template_loader = jinja2.FileSystemLoader(searchpath="./")
+        template_env = jinja2.Environment(loader=template_loader)
+        self.template = template_env.get_template(f"{INPUT_FOLDER_PATH}{resume_template}") 
+        
+    def build(self):
+        # Render the HTML template with the data
+        html_content = self.template.render(self.resume_data)
 
-# Load data from a JSON file
-with open(f"{INPUT_FOLDER_PATH}{RESUME_DATA_FILE}", "r") as data_file:
-    data = json.load(data_file)
-# with open("./src/resume_data.json", "r") as data_file:
-#     data = json.load(data_file)
+        # Create a PDF from the rendered HTML
+        HTML(string=html_content).write_pdf(f"{OUTPUT_FOLDER_PATH}{RESUME_FILE}") 
+        
 
-# Render the HTML template with the data
-html_content = template.render(data)
+if __name__ == "__main__":
+    resume_builder = ResumeBuilder()
+    resume_builder.build()
 
-# Create a PDF from the rendered HTML
-HTML(string=html_content).write_pdf(f"{OUTPUT_FOLDER_PATH}{RESUME_FILE}")
