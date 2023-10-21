2b2f1d26dd0314ffb0c58e09434a2a9105e94346
akshay.khanna <akshay991110263@gmail.com>
2023-10-21 11:27:23 +0530

    Make llama script run for multiple modals

--- src/constants.py
+++ src/constants.py
@@ -4,3 +4,13 @@ RESUME_TEMPLATE_FILE = "resume_template.html"
 RESUME_DATA_FILE = "resume_data.json"
 RESUME_HTML_FILE = "resume.html"
 RESUME_PDF_FILE = "resume.pdf"
+PORT_NO = 11434
+LOCAL_HOST = "http://localhost"
+MODELS = {
+    "llama": {
+        "name": "llama2",
+    },
+    "resume": {
+        "name": "resume-writer",
+    },
+}
--- src/llama-llm.py
+++ src/llama-llm.py
@@ -1,15 +1,21 @@
 from langchain.llms import Ollama
 from langchain.callbacks.manager import CallbackManager
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from constants import LOCAL_HOST, PORT_NO, MODELS
 
-llm = Ollama(
-    base_url="http://localhost:11434",
-    model="resume-writer",
-    callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]),
-)
 
-def generateResponse(prompt):
-    return llm(prompt);
+class LLM:
+    def __init__(self, model_name):
+        self.llm = Ollama(
+            base_url=f"{LOCAL_HOST}:{PORT_NO}",
+            model=model_name,
+            callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]),
+        )
+
+    def generateResponse(self, prompt):
+        return self.llm(prompt)
+
 
 if __name__ == "__main__":
-    generateResponse("Generate resume for Senior Full Stack Developer.")
+    llm = LLM(MODELS["resume"]["name"])
+    llm.generateResponse("Generate resume for Senior Full Stack Developer.")
